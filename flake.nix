39c51d71676b0dd78488f35f98494a99c232ab30
tall-josh <joshua.patterson@silverpond.com.au>
2026-04-09 22:57:47 +1000

    bug

--- flake.nix
+++ flake.nix
@@ -1,17 +1,41 @@
-outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-  in
-  {
-    packages.${system}.neovim =
-      let
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-      in
-      import ./neovim.nix pkgs;
+{
+  description = "Josh's NixOS system";
 
-    nixosConfigurations.luke = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./configuration.nix
-      ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      packages.${system}.neovim =
+        let
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        in
+        import ./neovim.nix pkgs;
+
+      nixosConfigurations.feifei = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+
+      devShells.${system}.python =
+        let
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            python313
+            uv
+            ruff
+            pyright
+            git
+            direnv
+          ];
+        };
     };
+}
